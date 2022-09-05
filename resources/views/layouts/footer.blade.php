bcf08d07df04a1cfb084823e455432a9ae8268e3
muhammadahmadjanjua <40795546+muhammadahmadjanjua@users.noreply.github.com>
2022-09-05 14:40:27 +0500

    Footer adjusted
    Text added for the footer and footer is adjusted

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,17 @@
+<div class="footer-text">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8">
+                <h4>Heading goes here</h4>
+                <p>Ut interdum justo tempus feugiat scelerisque. Nunc condimentum, metus quis elementum posuere, quam felis cursus justo, at viverra lorem ex ut magna. Donec nec efficitur tortor. Morbi bibendum leo eu ex laoreet efficitur. Proin a egestas purus, a cursus leo. Proin luctus quam sem, sit amet porta magna porttitor et. Curabitur justo nisl, eleifend sit amet pharetra ut, semper vel ipsum. Nulla porta nec ex non convallis. Praesent feugiat, turpis nec tincidunt condimentum, purus erat fringilla elit, vitae scelerisque arcu metus eu velit. Pellentesque aliquet nulla risus. Cras sollicitudin, odio a auctor lobortis, leo nisl tincidunt orci, viverra mattis lacus elit ac metus. Vivamus commodo tempor mauris et volutpat. Nulla libero ante, dapibus at diam id, luctus pulvinar lectus. Sed fringilla magna nulla, quis euismod libero ullamcorper sit amet. Donec malesuada ac tellus id viverra.</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Secondary Heading goes here</h4>
+                <p>Ut interdum justo tempus feugiat scelerisque. Nunc condimentum, metus quis elementum posuere, quam felis cursus justo, at viverra lorem ex ut magna. Donec nec efficitur tortor. Morbi bibendum leo eu ex laoreet efficitur. Proin a egestas purus, a cursus leo. </p>
+            </div>
+        </div>
+    </div>
+</div>
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
