7a6406393dfc3eb652aedaa3b06f7d599458cbe3
muhammadahmadjanjua <40795546+muhammadahmadjanjua@users.noreply.github.com>
2022-09-06 14:51:49 +0500

    js/css cleanup
    clean up assets, I combines all js and css files at one place and remove extra files

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -1,8 +1,2 @@
 @yield('css')
-
-<!-- Bootstrap Css -->
-<link href="{{ URL::asset('assets/css/bootstrap.min.css') }}"  rel="stylesheet" id="bootstrap-style" type="text/css" />
-<!-- Icons Css -->
-<link href="{{ URL::asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-<!-- App Css-->
 <link href="{{ URL::asset('assets/css/app.min.css') }}" rel="stylesheet" id="app-style" type="text/css" />
